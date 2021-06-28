8f64539d6ac4c29afc2ddcd425c5c1d6b417df3e
ROY-OFFICIAL <85698618+ROY-OFFICIAL@users.noreply.github.com>
2021-06-28 23:49:06 +0700

    Update ROY-YMBF.py

--- ROY-YMBF.py
+++ ROY-YMBF.py
@@ -394,7 +394,7 @@ class ngewe:
 					self.id.append(re.findall("uid\=(.*?)\&",softek[0])[0]+"<=>"+softek[1])
 				else:
 					self.id.append(re.findall("\/(.*?)\?fref",softek[0])[0]+"<=>"+softek[1])
-				print(f"\r %s[*] Sedang mengumpulkan %s{len(self.id)} %sID... ",end=""%(N,H,N))
+				print(f"\r [*] Sedang mengumpulkan {len(self.id)} ID... ",end="")
 			if "Lihat selengkapnya" in kontol:
 				self.teman(self.url+parser(kontol,"html.parser").find("a",string="Lihat selengkapnya").get("href"))
 		except:pass
